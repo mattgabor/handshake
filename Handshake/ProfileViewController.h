b81ffb6f617be70877ae80c72b5ecfaf9cad72ce
Shai Bruhis <shaibruhis@calvisitor-10-105-153-111.CalVisitor.1918.Berkeley.EDU>
2015-10-10 04:33:52 -0700

    login screen with segue to next vc

---
+++ Handshake/ProfileViewController.h
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.h
+//  Handshake
+//
+//  Created by Shai Bruhis on 10/10/15.
+//  Copyright © 2015 Prodigies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@property (nonatomic, strong) NSString *name;
+
+@end
