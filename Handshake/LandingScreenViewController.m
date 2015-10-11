5effc5086d679a6478346950ef915392367f3cdb
Shai Bruhis <shaibruhis@calvisitor-10-105-153-134.CalVisitor.1918.Berkeley.EDU>
2015-10-10 22:39:47 -0700

    turned selecting source type for image picker to be an uialertcontroller

--- Handshake/LandingScreenViewController.m
+++ Handshake/LandingScreenViewController.m
@@ -7,10 +7,11 @@
 //
 
 #import "LandingScreenViewController.h"
-#import "chooseImageSourceViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
 
-@interface LandingScreenViewController () <UITextFieldDelegate>
+
+@interface LandingScreenViewController () <UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *personTextField;
 @property (weak, nonatomic) IBOutlet UIButton *addPhotoButton;
@@ -66,20 +67,55 @@
 }
 
 #pragma mark - Actions
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([segue.identifier isEqualToString:@"chooseSourceSegue"]) {
-        chooseImageSourceViewController *destVC = (chooseImageSourceViewController *)segue.destinationViewController;
-        destVC.parent = self;
-    }
-}
-
 - (IBAction)addPhotoButtonAction:(UIButton *)sender
 {
-//    self.imagePickerController = [[UIImagePickerController alloc] init];
-//    self.imagePickerController.delegate = self;
-//    self.imagePickerController.mediaTypes = @[(NSString *)kUTTypeImage];
-//    self.imagePickerController.allowsEditing = YES;
+    // present alert controller
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:@"Choose Source" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+           [alertController dismissViewControllerAnimated:YES completion:nil];
+    }];
+
+    UIAlertAction *choosePhotoAction = [UIAlertAction actionWithTitle:@"Choose Photo" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        imagePickerController.delegate = self;
+        imagePickerController.mediaTypes = @[(NSString *)kUTTypeImage];
+        imagePickerController.allowsEditing = YES;
+        imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:imagePickerController animated:YES completion:NULL];
+    }];
+    
+    UIAlertAction *takePhotoAction = [UIAlertAction actionWithTitle:@"Take Photo" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        imagePickerController.delegate = self;
+        imagePickerController.mediaTypes = @[(NSString *)kUTTypeImage];
+        imagePickerController.allowsEditing = YES;
+        imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:imagePickerController animated:YES completion:NULL];
+    }];
+    
+    [alertController addAction:cancelAction];
+    [alertController addAction:choosePhotoAction];
+    [alertController addAction:takePhotoAction];
+    
+    [self presentViewController:alertController animated:YES completion:NULL];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    self.image = info[UIImagePickerControllerEditedImage];
+    if (!self.image)
+    {
+        self.image = info[UIImagePickerControllerOriginalImage];
+    }
+    [self.addPhotoButton setTitle:@"" forState:UIControlStateNormal];
+    [self.addPhotoButton setBackgroundImage:self.image forState:UIControlStateNormal];
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 - (IBAction)meetButtonAction:(UIButton *)sender
